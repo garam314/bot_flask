239b8cb14fbc9e116b11e052f08fec07970cd41b
Darío Tapia Chandía <dtapia@svti.cl>
2026-02-23 16:57:17 -0300

    Added New EndPoint

--- Bot/Consultas.py
+++ Bot/Consultas.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
+import json
 
 bp_request = Blueprint("request", __name__, url_prefix="/requests")
 
 
 @bp_request.route("/", methods=["POST"])
-def get_test():
-    return "Hola Mundo"
+def get_data(message: str):
+    message = (message or "").strip()
+    if not message:
+        return json.dumps({"code": 400, "msg": "Opcion no valida"})
